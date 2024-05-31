cec120d7bf2a8f02aca76940946248f53003d3c0
Sasha Waszaj <swaszajf@gmail.com>
2024-05-31 13:48:58 -0400

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("hello")
+
+var requestSpan = document.querySelector("#requests");
+var connectionSpan = document.querySelector("#connections");
+var username = document.querySelector("#username");
+
+function accept(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestSpan.innerText--;
+    connectionSpan.innerText++;
+}
+
+function decline(id) {
+    var element = document.querySelector(id);
+    element.remove();
+    requestSpan.innerText--;
+}
+
+function edit() {
+    username.innerText = "Sasha Waszaj";
+}
